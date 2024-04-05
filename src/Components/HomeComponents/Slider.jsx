ea4bd0882a314202f8bde612eecdfa89ba33fe3e
itsHarley01 <harlae116@gmail.com>
2024-04-05 11:50:12 +0800

    slides responsive update

--- src/Components/HomeComponents/Slider.jsx
+++ src/Components/HomeComponents/Slider.jsx
@@ -23,6 +23,7 @@ function NewsSlider() {
       </div>
     );
   };
+
   const [newsData, setNewsData] = useState([]);
   const [newsIndex, setNewsIndex] = useState(0);
 
@@ -37,25 +38,37 @@ function NewsSlider() {
     });
   }, []);
 
-  const settings = {
+  useEffect(() => {
+    const handleResize = () => {
+      const slidesToShow = window.innerWidth <= 768 ? 1 : 3; // Adjust as needed
+      setSettings({ ...settings, slidesToShow });
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const [settings, setSettings] = useState({
     infinite: true,
     lazyLoad: true,
     speed: 300,
-    slidesToShow: 3,
-    dots:true,
+    slidesToShow: 3, 
+    dots: true,
     centerMode: true,
     centerPadding: 0,
     nextArrow: <NextArrow />,
     prevArrow: <PrevArrow />,
     beforeChange: (current, next) => setNewsIndex(next),  
-  };
+  });
 
   return (
-    <div className="m-20 h-full"  >
+    <div className="m-20 h-full" >
       <h1 className='w-full text-center text-4xl mb-20 font-semibold'>Get Updated with the Barangay</h1>
       <Slider {...settings}>
         {newsData.map((newsItem, idx) => (
-          <div key={idx} className={idx === newsIndex ? "justify-center transform scale-100 opacity-100 transition-transform duration-300 w-full " : "justify-center opacity-50 trasform scale-[0.6] transition-transform duration-300"}>
+          <div key={idx} className={idx === newsIndex ? "justify-center transform scale-100 opacity-100 transition-transform duration-300 w-full" : "justify-center opacity-50 transform scale-[0.6] transition-transform duration-300"}>
             <NewsCard   
               image={newsItem.image}
               headerText={newsItem.title}
